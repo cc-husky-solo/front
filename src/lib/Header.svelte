43c21e13d5443f25854f740e47f808301c23ab57
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-13 10:36:38 +0000

    Feat: Header component done.
    Used Navbar from svelte components.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Husky Project
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+</Navbar>
